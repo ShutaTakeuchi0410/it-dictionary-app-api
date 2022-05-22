73ea076eae7f414eb804bb5cfe6b41d30c841226
ShutaTakeuchi0410 <takeuchi@hab-co.jp>
2022-05-22 17:59:47 +0900

    feat: e-wordsのスクレイピングで結果をjsonで出力

--- app/controllers/api/v1/terms_controller.rb
+++ app/controllers/api/v1/terms_controller.rb
@@ -1,9 +1,39 @@
 class Api::V1::TermsController < ApplicationController
   def index
     agent = Mechanize.new
-    page = agent.get("https://e-words.jp/w/Web_API.html")
-    elements = page.search('#Summary')
+    agent.user_agent = 'Mac Mozilla'
 
-    render json: elements.inner_text
+    # Google検索
+    agent.get('https://www.google.co.jp/')
+    agent.page.form_with(name: 'f') do |form|
+        form.q = "site:https://e-words.jp #{params[:keyword]}"
+    end.submit
+    
+    # 検索結果のリンク
+    google_links = agent.page.links
+
+    status = false
+    data = nil
+
+    google_links.each do |google_link|
+      if google_link.uri.to_s.start_with?('/url?q=https://e-words.jp/w/')
+        # 最初にヒットした「e-Words」のリンクをクリック
+        e_words_page = google_link.click
+
+        # 各種情報を取得
+        # NOTE: stripメソッドで特殊文字が存在してる場合に取りぞいています。
+        url = e_words_page.uri.to_s
+        name = e_words_page.search('//*[@id="content"]/article/h1/span[1]').inner_text.strip
+        summary = e_words_page.search('//*[@id="Summary"]').inner_text.strip
+        detail = e_words_page.search('//*[@id="content"]/article/p[1]').inner_text.strip
+
+        status = true
+        data = { url: url, name: name, summary: summary, detail: detail }
+
+        break
+      end
+    end
+
+    render json: { status: status, data: data }
   end
 end
